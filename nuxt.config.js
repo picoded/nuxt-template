d2126856a137dc7ec5464eb310771caa9ed07475
Eugene Cheah <eugeneqin@gmail.com>
2017-09-02 01:00:10 +0800

    Including bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,36 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       //{ hid: 'description', name: 'description', content: 'nuxt-project' }
     ],
+    script:[
+        {
+          src:"https://code.jquery.com/jquery-3.2.1.slim.min.js", 
+          integrity:"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN",
+          crossorigin:"anonymous"
+        },
+        {
+          src:"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js",
+          integrity:"sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4",
+          crossorigin:"anonymous"
+        },
+        {
+          src:"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js",
+          integrity:"sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1",
+          crossorigin:"anonymous"
+        }
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      { 
+        rel: 'stylesheet', 
+        href: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css",
+        integrity: "sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M",
+        crossorigin:"anonymous" 
+      },
+      { 
+        rel: 'stylesheet', 
+        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+        crossorigin:"anonymous" 
+      }
     ]
   },
   css: [
